db751e0cce772f221ca96a98f46350e4435d202a
taran511 <69810248+taran511@users.noreply.github.com>
2020-11-04 14:13:27 -0500

    Add files via upload

---
+++ LinkWClothing2.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Row, Container, Col } from 'react-bootstrap';
+
+function Link2() {
+    return (
+        <div>
+         <Container fluid>
+           <Row>
+            <Col>
+             <img src= "https://slimages.macys.com/is/image/MCY/products/9/optimized/14889929_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%"/>
+         <div>
+             Bar III
+             Tie-Neck Blouse
+                $34.99
+        </div>
+          </Col>
+            <Col>
+            <img src= "https://slimages.macys.com/is/image/MCY/products/5/optimized/17748795_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%"/>
+         <div>
+             Kasper
+             Ruffle-Front Blouse
+                $44.99
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/8/optimized/17556838_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%"/>
+         <div>
+             Tommy Hilfiger
+             Ruffled Split-Neck Blouse
+                $37.50
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/2/optimized/15369422_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%"/>
+         <div>
+            DKNY
+            Printed Split-Neck Logo Shirt
+                $27.93
+        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/1/optimized/17887151_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%"/>
+         <div>
+             DKNY
+             Long-Sleeve V-Neck Top
+                $99.50
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/0/optimized/18106030_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%"/>
+         <div>
+              Vince Camuto
+              Women's Long Sleeve Tie Neck Blouse
+                $79.50
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= " https://slimages.macys.com/is/image/MCY/products/6/optimized/17625276_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%"/>
+         <div>
+                Charter Club
+                Metallic-Print Cape Top
+                $69.50
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/6/optimized/17542386_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%"/>
+         <div>
+            Calvin Klein
+            Womens Ombré Plaid Shirt
+                $32.99
+        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+    )
+}
+
+export default Link2;
