987dc7ee652e9a1e6e07fcb08cfa9b4c3e165ee9
taran511 <69810248+taran511@users.noreply.github.com>
2020-11-04 13:13:03 -0500

    Add files via upload

---
+++ LinkWClothing.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Row, Container, Col } from 'react-bootstrap';
+
+function Link2() {
+    return (
+        <div>
+         <Container fluid>
+           <Row>
+            <Col>
+             <img src= "https://slimages.macys.com/is/image/MCY/products/4/optimized/17729794_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%"/>
+         <div>
+                Charter Club
+                Plaid One-Button blazer
+                $99.50
+        </div>
+          </Col>
+            <Col>
+            <img src= "https://slimages.macys.com/is/image/MCY/products/7/optimized/17708417_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%"/>
+         <div>
+                Charter Club
+                Plaid One-Button blazer
+                $110.99
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://www.macys.com/shop/product/bar-iii-notched-one-button-blazer-created-for-macys?ID=11302664" width="100%"/>
+         <div>
+                Tommy Hillfigure
+                Plaid One-Button blazer
+                $90.50
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/5/optimized/12038665_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%"/>
+         <div>
+                Bar III
+                Plaid One-Button blazer
+                $78.99
+        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/0/optimized/8938930_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%"/>
+         <div>
+                DKNY
+                Plaid One-Button blazer
+                $99.50
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/0/optimized/17625290_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%"/>
+         <div>
+                DKNY
+                Zippered Faux-Leather Jacket
+                $119.50
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/1/optimized/15725951_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%"/>
+         <div>
+                Tommy Hillfigure
+                Military Band Jacket
+                $62.99
+        </div>
+                    </Col>
+                    <Col>
+                    <img src= "https://slimages.macys.com/is/image/MCY/products/6/optimized/16845606_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%"/>
+         <div>
+                Tommy Hillfigure
+                Striped Band-Collar Jacket
+                $62.99
+        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+    )
+}
+
+export default Link2;
