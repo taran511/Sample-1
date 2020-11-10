98e282517be07e0c7d36a456e31dafd47fa41bac
taran511 <69810248+taran511@users.noreply.github.com>
2020-11-10 16:12:54 -0500

    Add files via upload

---
+++ MenShirts.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import { Row, Container, Col } from 'react-bootstrap';
+import {Link} from 'react-router-dom'
+
+function MenShirts() {
+    return (
+        <div>
+            <h1 className="text-center text-danger my-5">Men Shirts</h1>
+            <Container fluid>
+                <Row className="px-5 py-5">
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/4/optimized/16490394_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%" />
+                        <div>
+                        Sun + Stone
+                        Men's Check Shirt
+                        $22.00
+                        </div>
+                     <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                    <Col>
+                         <img src="https://slimages.macys.com/is/image/MCY/products/2/optimized/16358772_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%" />
+                        <div>
+                        Club Room
+                        Men's Performance Stretch Plaid Shirt
+                        $45.00
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/9/optimized/11480949_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%" />
+                        <div>
+                        Club Room
+                        Men's Solid Stretch Oxford Cotton Shirt
+                        $37.00
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                </Row >
+                <Row className="px-5 py-5">
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/1/optimized/9681061_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%" />
+                        <div>
+                        Club Room
+                        Men's Stretch Dot-Print Shirt                            
+                        $37.99
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/7/optimized/15964837_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%" />
+                        <div>
+                        Calvin Klein
+                        Men's Button-Up Shirt                           
+                        $47.00
+                        </div>          <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                        </Col>
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/1/optimized/8789631_fpx.tif?op_sharpen=1&wid=661&fit=fit,1&$filtersm$&fmt=webp" width="100%" />
+                        <div>
+                        Tommy Hilfiger
+                        Men's Maxwell Short-Sleeve                    
+                        $44.00
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                     </Col>
+                     </Row>
+                    <Row className="px-5 py-5">
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/7/optimized/8793357_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%" />
+                        <div>
+                        Tommy Hilfiger
+                        Men's Maxwell Short-Sleeve 
+                            $34.55
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/3/optimized/16941043_fpx.tif?op_sharpen=1&wid=354&hei=430&fit=fit,1&$filtersm$&fmt=webp" width="100%" />
+                        <div>
+                        Tommy Hilfiger
+                        Men's Custom-Fit Peter Solid Shirt
+                        $24.90
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/8/optimized/16940968_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%" />
+                        <div>
+                        Tommy Hilfiger
+                        Men's Custom-Fit Peter Solid Shirt
+                        $24.93
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                    </Row>
+                    <Row className="px-5 py-5">
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/6/optimized/17647236_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%" />
+                        <div>
+                        Calvin Klein
+                        Men's Birdseye 
+                        $24.93
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/6/optimized/17647236_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$" width="100%" />
+                        <div>
+                        Calvin Klein
+                        Men's Birdseye Button-Down
+                        $24.55
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                    <Col>
+                        <img src="https://slimages.macys.com/is/image/MCY/products/2/optimized/16661902_fpx.tif?op_sharpen=1&wid=591&fit=fit,1&$filtersm$&fmt=webp" width="100%" />
+                        <div>
+                        Calvin Klein
+                        Short Sleeve Cotton Shirt
+                            $35.55
+                        </div>
+                        <Link to='/cart'>  <Button className="mt-3" variant="primary" >
+        Add to Cart
+      </Button></Link> 
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+    )
+}
+
+export default MenShirts;
